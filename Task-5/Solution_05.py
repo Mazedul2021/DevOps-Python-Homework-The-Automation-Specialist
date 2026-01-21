2dd046fafd37f555f2b89bc7f868c6f835a3d13c
Mazedul2021 <mdmazedul0172@gmail.com>
2026-01-22 01:48:55 +0600

    Task-5

---
+++ Task-5/Solution_05.py
@@ -0,0 +1,11 @@
+vm = {
+    "id": "vm-101",
+    "ip": "192.168.1.10",
+    "status": "running",
+    "region": "us-east-1"
+}
+
+vm["status"] = "stopped"
+vm["instance_type"] = "t3.large"
+
+print(vm)
